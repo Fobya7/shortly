e4914dbe9cbaa2001d37a9c4b8a727a0b5046a8d
Fobya7 <66226017+Fobya7@users.noreply.github.com>
2022-09-08 16:21:24 +0200

    hero image

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -3,6 +3,9 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'hero',
     templateUrl: './hero.component.html',
-    styleUrls: [ './hero.component.scss' ]
+    styleUrls: [
+        './hero-theme.component.scss',
+        './hero-layout.component.scss'
+        ]
     })
 export class HeroComponent {}
